8326e42707b119eabf08b7c1cc1dc82d0b8f42a6
Nobonita Saha Niha <nobonitasaha042@gmail.com>
2026-06-15 12:11:13 +0600

    Update index page

--- src/layouts/SiteLayout.jsx
+++ src/layouts/SiteLayout.jsx
@@ -1,11 +1,18 @@
 import { Outlet } from "react-router-dom";
 
+import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
+
 const SiteLayout = () => {
   return (
-    <div className="min-h-screen w-full">
-      <main className="w-full">
+    <div className="flex min-h-screen w-full flex-col overflow-x-hidden">
+      <Navbar />
+
+      <main className="w-full flex-1">
         <Outlet />
       </main>
+
+      <Footer />
     </div>
   );
 };
